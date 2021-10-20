0a440dc8beccdc41b0700b2c64edca301b26df09
Shusei-Dev <matissem33@gmail.com>
2021-10-20 14:03:51 +0200

    Mise à jour du fichier main

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import pygame as pg
+import time
 from pygame.locals import *
 
 class OctoPy():
 
     def __init__(self):
+        """Init method of the main class, settings all parameters like fullscreen, fps, version and more..."""
         pg.init()
+
         self.fullscreen = False
+        self.name = "OctoPy RythmGame v"
+        self.version = 0.1
         self.game_size = (1280, 720)
+        self.fps = 60
+
+        self.previous_frame_time = 0
+        self.dt = 0
+
         screen = pg.display.set_mode(self.game_size, self.fullscreen)
         self.screen_size = screen.get_size()
+        pg.display.set_caption(self.name + self.version)
+
+        mainClock = pg.time.Clock()
+
+    def calculate_deltatime(self):
+        self.dt = time.time() - self.previous_frame_time
+        dt *= self.fps
+        self.previous_frame_time = time.time()
 
 
 MainClass = OctoPy( )
