642935e025dda56b9f77d75f8c14babf444eddea
Shusei-Dev <matissem33@gmail.com>
2021-11-15 13:06:44 +0100

    toto

--- src/Utils/FileManager.py
+++ src/Utils/FileManager.py
@@ -1,8 +1,16 @@
 import yaml
 
-class FileManager:
 
-    def get_yml_content(self, file_path):
-        with open(file_path) as file:
-            file_content = yaml.load(file, Loader = yaml.FullLoader)
-            return file_content
+def get_yml_content(file_path):
+    with open(file_path) as file:
+        file_content = yaml.load(file, Loader = yaml.FullLoader)
+        return file_content
+
+def write_yml_content(file_path, var):
+    with open(file_path, "w") as file:
+        yaml.dump(var, file)
+
+def change_yml_content(file_path, t_var, content_var):
+    file_content = get_yml_content(file_path)
+    file_content[t_var] = content_var
+    write_yml_content(file_path, file_content)
