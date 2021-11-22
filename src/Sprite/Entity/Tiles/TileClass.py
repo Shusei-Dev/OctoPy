f3daf8d7c258d053f4720a43810eefd29b973f7c
Shusei-Dev <matissem33@gmail.com>
2021-11-22 09:56:26 +0100

    Ajustement de l'initialisation du system de map du jeu

--- src/Sprite/Entity/Tiles/TileClass.py
+++ src/Sprite/Entity/Tiles/TileClass.py
@@ -3,11 +3,15 @@ from src.Sprite.Entity.EntityClass import *
 
 class TileClass:
 
-    def __init__(self, surface, img, name, pos):
+    def __init__(self, surface, img, name, pos, type):
 
         self.screen = surface
         self.img = img
         self.name = name
         self.pos = pos
+        self.tileType = ["note", "slider"]
+        if type in self.tileType:
+            self.type = type
+
 
         self.tileSprite = EntityClass(self.screen, self.name, self.img, self.pos, "KeyNote", None)
