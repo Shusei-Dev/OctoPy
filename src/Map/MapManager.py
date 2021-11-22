2fc88d9c231a3b1db94f34b50edbd7fd945b49b5
Shusei-Dev <matissem33@gmail.com>
2021-11-22 08:39:24 +0100

    Aled

--- src/Map/MapManager.py
+++ src/Map/MapManager.py
@@ -63,6 +63,10 @@ class MapManager:
             mapDataInfo = self.mapData.get("Map_Info")
             mapDataInfo[elements[0]] = elements[1]
 
+        for elements in loadingFile["content"]:
+            mapDataContent = self.mapData.get("Map_Content")
+            mapDataContent.append(elements)
+
         return self.mapData
 
     def getMapInfo(self, mapObj, t_var):
